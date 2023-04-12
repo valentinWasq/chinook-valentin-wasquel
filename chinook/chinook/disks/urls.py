c9a64f1fd456559b28295d309507d78f9943b0e7
zerustu <wasquel.valentin@gmail.com>
2023-04-12 12:31:11 +0200

    premiere version des views

---
+++ chinook/chinook/disks/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'disks'
+urlpatterns = [
+    # ex: /disks/
+    path('', views.IndexView.as_view(), name='Index'),
+    # ex: /disks/5/
+    path('<int:pk>/', views.detailView.as_view(), name='detail')
+]
--- chinook/chinook/disks/views.py
+++ chinook/chinook/disks/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse,HttpResponseRedirect
+from django.template import loader
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+
+from .models import Artist, Album, Track
+
+class IndexView(generic.ListView):
+    template_name = 'disks/Index.html'
+    context_object_name = 'all_Album'
+
+    def get_queryset(self):
+        return Album.objects.all
+
+class detailView(generic.DetailView):
+    template_name = 'disks/detail.html'
+    model = Album
